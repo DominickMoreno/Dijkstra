494cbdef4005d0590a4830016d85f32ee356bc36
Dominick Moreno <dominickmoreno92@gmail.com>
2013-09-14 20:46:14 -0400

    Commit 2, further commit testing
    Modifying source.cpp to test how to commit/publish.

--- Dijkstra/Source.cpp
+++ Dijkstra/Source.cpp
@@ -1,8 +1,20 @@
+/* Author: Dominick Moreno
+** Project: Dijkstra
+** E-mail: Dominickmoreno92@gmail.com
+** linkedin: www.linkedin.com/pub/dominick-moreno/65/2a6/841/
+**
+** General code info here
+**
+*/
+
 #include <iostream>
 using namespace std;
 
 int main(){
-	cout << "Hello, world!\n";
+	int x;
+	x = 4 + 4;
+	cout << "This program reads maps in a defined folder, and finds the shortest path between two points.\n";
+	cout << "4 + 4 is: " << x << "\n";
 
 	system("pause");
 	return 0;
